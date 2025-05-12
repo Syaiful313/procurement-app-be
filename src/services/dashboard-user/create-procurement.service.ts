318dc06be6ae480023759b00394bb9baad2a34f3
Muhammad Syaiful Mu'min <143513644+Syaiful313@users.noreply.github.com>
2025-05-13 00:42:59 +0700

    Add department and date fields to Procurement model and update createProcurementService (#7)

--- src/services/dashboard-user/create-procurement.service.ts
+++ src/services/dashboard-user/create-procurement.service.ts
@@ -1,9 +1,12 @@
 import prisma from "../../config/prisma";
 import { ApiError } from "../../utils/api-error";
+import { Department } from "@prisma/client";
 
 interface ProcurementBody {
   username: string;
   description: string;
+  date: Date;
+  department: Department;
 }
 
 export const createProcurementService = async (
@@ -24,9 +27,18 @@ export const createProcurementService = async (
     if (!body.username) {
       throw new ApiError(400, "Username tidak boleh kosong");
     }
-    
+
     if (!body.description) {
       throw new ApiError(400, "Deskripsi tidak boleh kosong");
+    }
+
+    if (!body.department) {
+      throw new ApiError(400, "Department tidak boleh kosong");
+    }
+
+    const validDepartments = ["PURCHASE", "FACTORY", "OFFICE"];
+    if (!validDepartments.includes(body.department)) {
+      throw new ApiError(400, "Department tidak valid");
     }
 
     const result = await prisma.procurement.create({
@@ -34,6 +46,8 @@ export const createProcurementService = async (
         userId: user.id,
         username: body.username,
         description: body.description,
+        date: body.date,
+        department: body.department,
         status: "WAITING_CONFIRMATION",
       },
     });
